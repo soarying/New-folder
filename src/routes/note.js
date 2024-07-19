c74a2ae9848cd5d08221075b398d2c91964d9df5
Soar <soar_ding@live.ca>
2024-07-19 17:56:34 -0600

    update

--- src/routes/note.js
+++ src/routes/note.js
@@ -2,12 +2,13 @@ const express = require("express");
 const noteController = require("../controllers/note");
 const authenticateToken = require("../middlewares/all");
 const router = express.Router();
-router.use(authenticateToken);
-router.get("/note", noteController.getAllNotes);
-router.get("/note/:id", noteController.getNoteById);
-router.post("/note", noteController.createNote);
-router.post("/share", noteController.shareNote);
-router.put("/note", noteController.updateNote);
-router.delete("/note/:id", noteController.deleteNote);
-router.get("/search", noteController.searchNotes);
+
+router.get("/note", authenticateToken, noteController.getAllNotes);
+router.get("/note/:id", authenticateToken, noteController.getNoteById);
+router.post("/note", authenticateToken, noteController.createNote);
+router.post("/share", authenticateToken, noteController.shareNote);
+router.put("/note", authenticateToken, noteController.updateNote);
+router.delete("/note/:id", authenticateToken, noteController.deleteNote);
+router.get("/search", authenticateToken, noteController.searchNotes);
+
 module.exports = router;
